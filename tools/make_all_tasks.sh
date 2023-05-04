681fa2e93c5bfe433b9426e87dd33c0aa650b5c3
Laszlo Nikhazy <laszlo.nikhazy@gmail.com>
2023-05-04 21:06:14 +0200

    helper script for running task-maker everywhere

---
+++ tools/make_all_tasks.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+# should be called from the root of the repo
+
+cd topics
+for dir in */
+do
+    if [ -d "$dir" ]; then
+        cd "$dir"
+        for subdir in */
+        do
+            if [ -d "$subdir" ]; then
+                cd "$subdir"
+                echo "$subdir"
+                task-maker-rust
+                cd ..
+            fi
+        done
+        cd ..
+    fi
+done
+cd ..
+
