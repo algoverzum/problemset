264353018130db7d506a5ac4f1b557ce52d992af
Laszlo Nikhazy <laszlo.nikhazy@gmail.com>
2024-07-23 07:56:20 +0200

    Demo, wrong solution.

---
+++ topics/input-variables/total-fruits/sol/wrong.cpp
@@ -0,0 +1,10 @@
+// @check-wrong-answer: *
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a, b, c;
+    cin >> a >> b >> c;
+    cout << a + b + c + 1 << "\n";
+    return 0;
+}
