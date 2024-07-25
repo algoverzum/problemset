d3cd51ddcb6e6052371c3951e17d7107afd85d7f
PeterCsorba <149621051+PeterCsorba@users.noreply.github.com>
2024-07-25 20:20:47 +0200

    python solution

---
+++ topics/input-variables/global-warming/sol/solution_py.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python3
+# @check-accepted: *
+
+t = int(input())
+print(t-1,t+1)
---
+++ topics/input-variables/global-warming/sol/wrong.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python3
+# @check-wrong-answer: examples all
+
+t = int(input())
+print(t+1,t-1)
