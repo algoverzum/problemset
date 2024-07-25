77ac70763b7a3c0f0f379b268042cec21d3114a6
PeterCsorba <149621051+PeterCsorba@users.noreply.github.com>
2024-07-25 23:33:10 +0200

    python solution

---
+++ topics/input-variables/pingpong-match-length/sol/solution_py.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+# @check-accepted: *
+
+SH = int(input())
+SM = int(input())
+EH = int(input())
+EM = int(input())
+print(60 * EH + EM - 60 * SH - SM)
---
+++ topics/input-variables/pingpong-match-length/sol/wrong.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+# @check-wrong-answer: *
+
+SH = int(input())
+SM = int(input())
+EH = int(input())
+EM = int(input())
+print(EH + EM - SH - EM)
