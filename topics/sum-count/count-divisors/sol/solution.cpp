2add75dcdf4a1bfdb8d91ea533d137e0797f10b2
levus2002 <levus2002@gmail.com>
2024-09-03 10:35:41 +0200

    osztok->divisors

--- topics/sum-count/count-divisors/sol/solution.cpp
+++ topics/sum-count/count-divisors/sol/solution.cpp
@@ -5,11 +5,11 @@ using namespace std;
 int main() {
   int n;
   cin >> n;
-  int osztok = 0;
+  int divisors = 0;
   for (int i = 1; i <= n; i++) {
     if (n % i == 0) {
-      osztok++;
+      divisors++;
     }
   }
-  cout << osztok << "\n";
+  cout << divisors << "\n";
 }
--- topics/sum-count/count-divisors/sol/solution_py.py
+++ topics/sum-count/count-divisors/sol/solution_py.py
@@ -2,8 +2,8 @@
 # @check-accepted: *
 
 n = int(input())
-osztok = 0
+divisors = 0
 for i in range(1, n + 1):
     if n % i == 0:
-        osztok = osztok + 1
-print(osztok)
+        divisors = divisors + 1
+print(divisors)
