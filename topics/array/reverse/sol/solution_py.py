0dc8c87f443f70679cf83a3e35ee275a3898fb9e
Laszlo Nikhazy <laszlo.nikhazy@gmail.com>
2024-09-24 11:01:14 +0200

    small fixes

--- topics/array/reverse/sol/solution_py.py
+++ topics/array/reverse/sol/solution_py.py
@@ -1,12 +1,9 @@
 #!/usr/bin/env python3
 # @check-accepted: *
 
-numbers = list(map(int, input().split()))
-# print(*numbers)
-# print(*numbers[::-1])
-for i in range(12):
+numbers = [int(x) for x in input().split()]
+for i in range(13):
     print(numbers[i], end=" ")
-print(numbers[12])
-for i in range(12, 0, -1):
+print()
+for i in range(12, -1, -1):
     print(numbers[i], end=" ")
-print(numbers[0])
