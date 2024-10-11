4ff844819a9f0844ad240022692b599805ee3257
levus2002 <levus2002@gmail.com>
2024-10-11 15:54:31 +0200

    kthancestor solutions

--- topics/sparse-table/kth-ancestor/sol/solution.cpp
+++ topics/sparse-table/kth-ancestor/sol/solution.cpp
@@ -1,9 +1,48 @@
-// @check-accepted: *
-#include <iostream>
+#include <bits/stdc++.h>
 using namespace std;
+
+int kthancestor(vector<vector<int>> &v, int x, int k) {
+    for (int i = 0; i < 19 && x != -1; i++) {
+        if (k & (1 << i)) {
+            x = v[x][i];
+        }
+    }
+    return x;
+}
 
 int main() {
     int n;
     cin >> n;
-    cout << 42 << "\n";
+    vector<vector<int>> v(n + 1, vector<int>(19, -1));
+    int q;
+    cin >> q;
+    int type, y, x, k;
+
+    for (int i = 0; i < q; i++) {
+        cin >> type;
+        if (type == 0) {
+            cin >> y >> x;
+            v[x][0] = y;
+            for (int j = 1; j < 19; j++) {
+                if (v[x][j - 1] != -1) {
+                    v[x][j] = v[v[x][j - 1]][j - 1];
+                } else {
+                    break;
+                }
+            }
+        } else if (type == 1) {
+            cin >> x;
+            v[x][0] = -1;
+            for (int j = 1; j < 19; j++) {
+                v[x][j] = -1;
+            }
+        } else if (type == 2) {
+            cin >> x >> k;
+            int ancestor = kthancestor(v, x, k);
+            if (ancestor == -1)
+                ancestor = 0;
+            cout << ancestor << endl;
+        }
+    }
+    return 0;
 }
--- topics/sparse-table/kth-ancestor/sol/solution_py.py
+++ topics/sparse-table/kth-ancestor/sol/solution_py.py
@@ -1,5 +1,36 @@
-#!/usr/bin/env python3
-# @check-accepted: *
+def kthancestor(v, x, k):
+    for i in range(19):
+        if x == -1:
+            break
+        if k & (1 << i):
+            x = v[x][i]
+    return x
+
 
 n = int(input())
-print(42)
+v = [[-1] * 19 for i in range(n + 1)]
+q = int(input())
+
+for i in range(q):
+    query = list(map(int, input().split()))
+    type = query[0]
+
+    if type == 0:
+        y, x = query[1], query[2]
+        v[x][0] = y
+        for j in range(1, 19):
+            if v[x][j - 1] != -1:
+                v[x][j] = v[v[x][j - 1]][j - 1]
+            else:
+                break
+    elif type_ == 1:
+        x = query[1]
+        v[x][0] = -1
+        for j in range(1, 19):
+            v[x][j] = -1
+    elif type == 2:
+        x, k = query[1], query[2]
+        ancestor = kthancestor(v, x, k)
+        if ancestor == -1:
+            ancestor = 0
+        print(ancestor)
