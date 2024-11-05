50213e639838ed929a2a1ee45ec962ea161ad8f6
Laszlo Nikhazy <laszlo.nikhazy@gmail.com>
2024-11-05 14:30:45 +0100

    hints

--- topics/array/bets/sol/solution.cpp
+++ topics/array/bets/sol/solution.cpp
@@ -4,25 +4,25 @@
 using namespace std;
 
 int main() {
-    int N;
-    cin >> N;
-    vector<int> A(N);
-    for (int i = 0; i < N; ++i) {
-        cin >> A[i];
+    int n;
+    cin >> n;
+    vector<int> money(n);
+    for (int i = 0; i < n; ++i) {
+        cin >> money[i];
     }
-    vector<int> win(N - 1);
-    for (int i = 0; i < N - 1; ++i) {
-        win[i] = A[i + 1] - A[i];
+    vector<int> win(n - 1);
+    for (int i = 0; i < n - 1; ++i) {
+        win[i] = money[i + 1] - money[i];
     }
-    vector<int> diff(N - 2);
-    for (int i = 0; i < N - 2; ++i) {
+    vector<int> diff(n - 2);
+    for (int i = 0; i < n - 2; ++i) {
         diff[i] = win[i + 1] - win[i];
     }
-    for (int i = 0; i < N - 1; ++i) {
+    for (int i = 0; i < n - 1; ++i) {
         cout << win[i] << " ";
     }
     cout << "\n";
-    for (int i = 0; i < N - 2; ++i) {
+    for (int i = 0; i < n - 2; ++i) {
         cout << diff[i] << " ";
     }
     cout << "\n";
--- topics/array/bets/sol/solution_py.py
+++ topics/array/bets/sol/solution_py.py
@@ -1,13 +1,13 @@
 #!/usr/bin/env python3
 # @check-accepted: *
 
-N = int(input())
-A = [int(x) for x in input().split()]
+n = int(input())
+money = [int(x) for x in input().split()]
 win = []
-for i in range(N - 1):
-    win.append(A[i + 1] - A[i])
+for i in range(n - 1):
+    win.append(money[i + 1] - money[i])
 diff = []
-for i in range(N - 2):
+for i in range(n - 2):
     diff.append(win[i + 1] - win[i])
 print(*win)
 print(*diff)
