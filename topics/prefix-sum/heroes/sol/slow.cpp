ca35136b121a49edc20374303fe119983ed6e856
Laszlo Nikhazy <laszlo.nikhazy@gmail.com>
2025-01-22 08:23:17 +0100

    slow solution

---
+++ topics/prefix-sum/heroes/sol/slow.cpp
@@ -0,0 +1,23 @@
+// @check-accepted: *
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() {
+    const int m = 1000000;
+    int n;
+    cin >> n;
+    vector<int> cnt(m + 2);
+    for (int i = 0; i < n; i++) {
+        int arr, leave;
+        cin >> arr >> leave;
+        for (int j = arr; j <= leave; j++) {
+            cnt[j] += 1;
+        }
+    }
+    int ans = 0;
+    for (int i = 1; i <= m; i++) {
+        ans = max(ans, cnt[i]);
+    }
+    cout << ans << "\n";
+}
