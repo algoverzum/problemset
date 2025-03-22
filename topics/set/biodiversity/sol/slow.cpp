20e1e14a1eec4a822a462ffeb28e970fb6ad3246
levus2002 <levus2002@gmail.com>
2025-03-22 22:23:37 +0100

    slow solution for trading and biodiversity

---
+++ topics/set/biodiversity/sol/slow.cpp
@@ -0,0 +1,50 @@
+#include <algorithm>
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() {
+    int N, M;
+    cin >> N >> M;
+    vector<int> unique_ids;
+    for (int i = 0; i < N; ++i) {
+        int x;
+        cin >> x;
+
+        bool found = false;
+        for (int y : unique_ids) {
+            if (y == x) {
+                found = true;
+                break;
+            }
+        }
+
+        if (!found)
+            unique_ids.push_back(x);
+    }
+    for (int i = 0; i < M; ++i) {
+        int x;
+        cin >> x;
+
+        bool found = false;
+        for (int y : unique_ids) {
+            if (y == x) {
+                found = true;
+                break;
+            }
+        }
+
+        if (!found)
+            unique_ids.push_back(x);
+    }
+    sort(unique_ids.begin(), unique_ids.end());
+    cout << unique_ids.size() << "\n";
+    for (size_t i = 0; i < unique_ids.size(); ++i) {
+        if (i > 0)
+            cout << " ";
+        cout << unique_ids[i];
+    }
+    cout << "\n";
+
+    return 0;
+}
---
+++ topics/set/traders/sol/slow.cpp
@@ -0,0 +1,76 @@
+#include <algorithm>
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int main() {
+    int N, M;
+    cin >> N;
+    vector<string> fruits1;
+
+    for (int i = 0; i < N; ++i) {
+        string fruit;
+        cin >> fruit;
+        bool found = false;
+        for (const string &f : fruits1) {
+            if (f == fruit) {
+                found = true;
+                break;
+            }
+        }
+
+        if (!found)
+            fruits1.push_back(fruit);
+    }
+
+    cin >> M;
+    vector<string> fruits2;
+
+    for (int i = 0; i < M; ++i) {
+        string fruit;
+        cin >> fruit;
+        bool found = false;
+        for (const string &f : fruits2) {
+            if (f == fruit) {
+                found = true;
+                break;
+            }
+        }
+        if (!found)
+            fruits2.push_back(fruit);
+    }
+
+    vector<string> exclusive;
+    for (const string &f1 : fruits1) {
+        bool found = false;
+        for (const string &f2 : fruits2) {
+            if (f1 == f2) {
+                found = true;
+                break;
+            }
+        }
+        if (!found)
+            exclusive.push_back(f1);
+    }
+
+    for (const string &f2 : fruits2) {
+        bool found = false;
+        for (const string &f1 : fruits1) {
+            if (f1 == f2) {
+                found = true;
+                break;
+            }
+        }
+        if (!found)
+            exclusive.push_back(f2);
+    }
+    sort(exclusive.begin(), exclusive.end());
+    cout << exclusive.size() << "\n";
+    for (const string &fruit : exclusive) {
+        cout << fruit << "\n";
+    }
+
+    return 0;
+}
