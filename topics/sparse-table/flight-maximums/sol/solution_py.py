8f1975188b76c4b13ba074e6ca57b4d8276afb66
PeterCsorba <platyp2@gmail.com>
2025-03-30 00:15:29 +0100

    [flight-maximums] solution.py time limit might be too small

--- topics/sparse-table/flight-maximums/sol/solution_py.py
+++ topics/sparse-table/flight-maximums/sol/solution_py.py
@@ -1,8 +1,28 @@
 #!/usr/bin/env python3
-# @check-accepted: examples small
+# @check-accepted: *
+from sys import stdin
 
-n, q = map(int, input().split())
-h = [0] + list(map(int, input().split()))
-for i in range(q):
-    s, k = map(int, input().split())
-    print(max(h[s : s + 2**k]))
+input = stdin.readline
+
+
+def solv():
+    n, q = map(int, input().split())
+    lg = [0] * (n + 1)
+    for i in range(2, n + 1):
+        lg[i] = lg[i // 2] + 1
+    st = [[0] * (n + 1) for _ in range(18)]
+
+    arr = list(map(int, input().split()))
+    for i in range(1, n + 1):
+        st[0][i] = arr[i - 1]
+
+    for i in range(1, lg[n] + 1):
+        for j in range(1, n - 2**i + 2):
+            st[i][j] = max(st[i - 1][j], st[i - 1][j + 2 ** (i - 1)])
+
+    for _ in range(q):
+        s, k = map(int, input().split())
+        print(st[k][s])
+
+
+solv()
