b033e74c72d7bd02f758f71ea3e64a3114ed1345
Laszlo Nikhazy <laszlo.nikhazy@gmail.com>
2025-06-15 21:16:24 +0200

    review and hints

--- topics/range-dp/longest-substring/sol/solution.cpp
+++ topics/range-dp/longest-substring/sol/solution.cpp
@@ -4,15 +4,17 @@
 #include <vector>
 using namespace std;
 
-int longest_common_substring(const string &s1, const string &s2) {
-    int m = s1.length(), n = s2.length();
+int longest_common_substring(string a, string b) {
+    a = "#" + a;
+    b = "$" + b;
+    int n = a.length(), m = b.length();
     int max_len = 0;
 
-    vector<vector<int>> dp(m + 1, vector<int>(n + 1, 0));
+    vector<vector<int>> dp(n, vector<int>(m, 0));
 
-    for (int i = 1; i <= m; ++i) {
-        for (int j = 1; j <= n; ++j) {
-            if (s1[i - 1] == s2[j - 1]) {
+    for (int i = 1; i < n; ++i) {
+        for (int j = 1; j < m; ++j) {
+            if (a[i] == b[j]) {
                 dp[i][j] = dp[i - 1][j - 1] + 1;
                 max_len = max(max_len, dp[i][j]);
             }
