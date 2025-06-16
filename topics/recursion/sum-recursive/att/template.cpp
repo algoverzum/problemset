28c41f7ce21a7995b2a026afb1c17be4a9816d7e
Laszlo Nikhazy <laszlo.nikhazy@gmail.com>
2025-06-16 15:24:34 +0200

    fix cpp template

--- topics/recursion/sum-recursive/att/template.cpp
+++ topics/recursion/sum-recursive/att/template.cpp
@@ -3,12 +3,11 @@ using namespace std;
 
 long long summa(int a, int b) {
     // Write your code here
-    return 0;
 }
 
 // Do not change anything below.
 int main() {
     int a, b;
     cin >> a >> b;
-    cout << summa(a, b) << "/n";
+    cout << summa(a, b) << "\n";
 }
