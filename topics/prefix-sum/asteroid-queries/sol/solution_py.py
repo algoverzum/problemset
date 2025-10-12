29321bf144005ae489b9b75cc5223c278122e0d2
PeterCsorba <platyp2@gmail.com>
2025-10-12 15:44:07 +0200

    [asteroid-queries] new python sol

--- topics/prefix-sum/asteroid-queries/sol/solution_py.py
+++ topics/prefix-sum/asteroid-queries/sol/solution_py.py
@@ -1,18 +1,22 @@
 #!/usr/bin/env python3
 # @check-accepted: *
 
-n, q = [int(x) for x in input().split()]
-a = [int(x) for x in input().split()]
+import sys
+
+input = sys.stdin.readline
+
+n, q = map(int, input().split())
+A = list(map(int, input().split()))
 
 prefix_sum = [0] * (n + 1)
-for i in range(1, n + 1):
-    prefix_sum[i] = prefix_sum[i - 1] + a[i - 1]
+for i in range(n):
+    prefix_sum[i + 1] = prefix_sum[i] + A[i]
 
 for _ in range(q):
-    line = input().split()
-    if line[0] == "1":
-        k = int(line[1])
+    t, *rest = map(int, input().split())
+    if t == 1:
+        k = rest[0]
         print(prefix_sum[k])
     else:
-        l, r = int(line[1]), int(line[2])
+        l, r = rest
         print(prefix_sum[r] - prefix_sum[l - 1])
