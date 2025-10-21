b28b7aa08494cc8fc1557e49b58d2c8b555384e0
Laszlo Nikhazy <laszlo.nikhazy@gmail.com>
2025-10-21 11:09:45 +0200

    Hints and cpp solution fix

--- topics/if-then-else/rock-paper-scissors/sol/solution.cpp
+++ topics/if-then-else/rock-paper-scissors/sol/solution.cpp
@@ -5,14 +5,13 @@ using namespace std;
 int main() {
     string tipp1, tipp2;
     cin >> tipp1 >> tipp2;
-    if (tipp1 == tipp2)
-        cout << 0 << "\n";
-    if (tipp1 == "ko" and tipp2 == "ollo" or
-        tipp1 == "ollo" and tipp2 == "papir" or
-        tipp1 == "papir" and tipp2 == "ko")
-        cout << 1 << "\n";
-    if (tipp1 == "ko" and tipp2 == "papir" or
-        tipp1 == "papir" and tipp2 == "ollo" or
-        tipp1 == "ollo" and tipp2 == "ko")
-        cout << 2 << "\n";
+    if (tipp1 == tipp2) {
+        cout << "0\n";
+    } else if (tipp1 == "ko" && tipp2 == "ollo" ||
+               tipp1 == "ollo" && tipp2 == "papir" ||
+               tipp1 == "papir" && tipp2 == "ko") {
+        cout << "1\n";
+    } else {
+        cout << "2\n";
+    }
 }
