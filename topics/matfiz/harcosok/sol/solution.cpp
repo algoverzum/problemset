b6774bb3e7862d12e1c60426f7aafd8491ff4e0e
Laszlo Nikhazy <laszlo.nikhazy@gmail.com>
2026-01-27 10:53:05 +0100

    [harcosok] review

--- topics/matfiz/harcosok/sol/solution.cpp
+++ topics/matfiz/harcosok/sol/solution.cpp
@@ -5,18 +5,18 @@
 using namespace std;
 
 int main() {
-    int N;
-    cin >> N;
-    vector<int> A(N);
-    for (int i = 0; i < N; ++i) {
-        cin >> A[i];
+    int n;
+    cin >> n;
+    vector<int> warrior(n);
+    for (int i = 0; i < n; ++i) {
+        cin >> warrior[i];
     }
 
-    sort(A.begin(), A.end());
+    sort(warrior.begin(), warrior.end());
 
     int ans = 0;
-    for (int i = 0; i < N / 2; ++i) {
-        ans += A[2 * i + 1] - A[2 * i];
+    for (int i = 1; i < n; i += 2) {
+        ans += warrior[i] - warrior[i - 1];
     }
 
     cout << ans << '\n';
--- topics/matfiz/harcosok/sol/solution_py.py
+++ topics/matfiz/harcosok/sol/solution_py.py
@@ -5,6 +5,6 @@ N = int(input())
 A = [int(x) for x in input().split()]
 A.sort()
 ans = 0
-for i in range(N // 2):
-    ans += A[2 * i + 1] - A[2 * i]
+for i in range(1, N, 2):
+    ans += A[i] - A[i - 1]
 print(ans)
