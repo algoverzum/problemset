f4dbf5c7a77a016dfb6c9b2e3fd5ca2fe5e61007
PeterCsorba <platyp2@gmail.com>
2026-05-02 17:19:33 +0200

    [min-max/cheapest-long-travel] add wrong solution and testcases for it

---
+++ topics/min-max/cheapest-long-travel/sol/wrong.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+n, k = [int(x) for x in input().split()]
+distance = [int(x) for x in input().split()]
+price = [int(x) for x in input().split()]
+
+cheapest = -1
+
+for i in range(n):
+    if distance[i] > k:
+        if cheapest == -1 or cheapest > price[i]:
+            cheapest = price[i]
+
+print(cheapest)
